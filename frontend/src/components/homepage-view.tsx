fcb3b0c36dc9b6da55b017d2194fcb4afba65447
Matti Sebastian Fischbach <matti.fischbach@student.uibk.ac.at>
2026-03-09 21:27:12 +0100

    watchtower test

--- frontend/src/components/homepage-view.tsx
+++ frontend/src/components/homepage-view.tsx
@@ -13,7 +13,6 @@ export const HomepageView: React.FC = () => {
 
     return (
         <div className="flex flex-column gap-2">
-            <Header title="Testing" />
             <Header title="Offene Rechnungen"/>
             <InvoiceTable invoices={openInvoices} isLoading={openInvoicesLoading} />
         </div>
