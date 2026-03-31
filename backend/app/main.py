7256095ed936bc74c0f7c1d1167ac1bb623830b8
Matti Sebastian Fischbach <matti.fischbach@student.uibk.ac.at>
2026-03-31 15:06:57 +0200

    backend logging

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,15 +5,24 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from .utilities.logger_setup import setup_global_logging
+
+setup_global_logging()
+logger = logging.getLogger("uvicorn.error")
+
 from .utilities.database import SessionLocal
 from .utilities.router_include import auto_include_routers
 from .utilities.seed import seed_users
 
-logger = logging.getLogger("uvicorn.error")
+try:
+    __version__ = metadata.version("invoice-backend")
+except metadata.PackageNotFoundError:
+    __version__ = "Not found"
 
 
 @asynccontextmanager
 async def lifespan(_app: FastAPI):
+    logger.info("Backend version: %s", __version__)
     db = SessionLocal()
     try:
         seed_users(db)
@@ -21,13 +30,6 @@ async def lifespan(_app: FastAPI):
         db.close()
     yield
 
-
-try:
-    __version__ = metadata.version("invoice-backend")
-except metadata.PackageNotFoundError:
-    __version__ = "Not found"
-
-logger.info("Backend version: %s", __version__)
 
 app = FastAPI(
     lifespan=lifespan,
---
+++ backend/app/utilities/logger_setup.py
@@ -0,0 +1,25 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+from ..utilities import LOG_PATH
+
+
+def setup_global_logging():
+    logger = logging.getLogger("uvicorn.error")
+
+    logger.setLevel(logging.INFO)
+
+    file_handler = RotatingFileHandler(
+        LOG_PATH,
+        maxBytes=5 * 1024 * 1024,  # 5 MB
+        backupCount=3  # Keep 3 backups
+    )
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    file_handler.setFormatter(formatter)
+
+    if not any(isinstance(h, RotatingFileHandler) for h in logger.handlers):
+        logger.addHandler(file_handler)
